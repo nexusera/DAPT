24f433c60c296e8a500c56faef39bcfd710b3f7c
HaoLi <1045576345@qq.com>
2026-01-04 09:25:12 +0800

    debug

--- add_noise_features.py
+++ add_noise_features.py
@@ -8,7 +8,6 @@ from noise_embeddings import NoiseFeatureExtractor
 
 # 默认 OCR 源（可覆盖）。按顺序拼接，后面的追加在前面之后。
 DEFAULT_OCR_JSONS = [
-    "/data/ocean/semi_label/ocr_rerun/ocr_noise/data/ocr_with_location_2300_combined.json",
     "/data/ocean/semi_label/ocr_rerun/char_ocr_9297.json",
 ]
 
